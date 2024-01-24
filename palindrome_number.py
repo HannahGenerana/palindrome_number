b2122684b525813d8812d19db951646ea29be034
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 22:02:02 +0800

    pseudocode

---
+++ palindrome_number.py
@@ -0,0 +1,11 @@
+# Exercise 9: Check Palindrome Number
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+
+# pseudocode
+
+# convert the number to a string
+
+# check if the string is equal to its reverse
+
+# print the result
